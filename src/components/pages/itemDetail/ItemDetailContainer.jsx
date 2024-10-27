3ee1ad1bf562729cfe07a44f5fff372fd7558dab
ricardo <rikardocqa21@gmail.com>
2024-10-27 18:14:52 -0300

    subida clase 10

---
+++ src/components/pages/itemDetail/ItemDetailContainer.jsx
@@ -0,0 +1,24 @@
+import "./itemDetailContainer.css";
+import { useEffect, useState } from "react";
+import { products } from "../../../products";
+import ItemDetail from "./ItemDetail";
+import { useParams } from "react-router-dom";
+
+const ItemDetailContainer = () => {
+  const { id } = useParams();
+  const [item, setItem] = useState({});
+
+  useEffect(() => {
+    let productSelected = products.find((producto) => producto.id === id);
+    setItem(productSelected);
+  }, [id]);
+
+  const agregarAlCarrito = (cantidad) => {
+    let objeto = { ...item, quantity: cantidad };
+    console.log(objeto);
+  };
+  <div className="card-container"></div>;
+  return <ItemDetail item={item} agregarAlCarrito={agregarAlCarrito} />;
+};
+
+export default ItemDetailContainer;
